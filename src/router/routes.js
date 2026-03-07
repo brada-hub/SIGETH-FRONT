a48c139e27e29fd54fc5f16b01dc347f2f5661c6
Brayan Padilla <padillasilesbrayandavid@gmail.com>
2026-03-06 22:53:39 -0400

    fix: aceptar nombres de permisos gestionar_* en rutas SIGETH

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,10 +4,10 @@ const routes = [
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/IndexPage.vue') },
-      { path: 'users', component: () => import('pages/UsersPage.vue'), meta: { permissions: ['usuarios', 'all'] } },
-      { path: 'applications', component: () => import('pages/ApplicationsPage.vue'), meta: { permissions: ['sistemas', 'all'] } },
-      { path: 'sedes', component: () => import('pages/SedesPage.vue'), meta: { permissions: ['sedes', 'all'] } },
-      { path: 'roles', component: () => import('pages/RolesPage.vue'), meta: { permissions: ['roles', 'all'] } },
+      { path: 'users', component: () => import('pages/UsersPage.vue'), meta: { permissions: ['usuarios', 'gestionar_usuarios', 'all'] } },
+      { path: 'applications', component: () => import('pages/ApplicationsPage.vue'), meta: { permissions: ['sistemas', 'gestionar_aplicaciones', 'all'] } },
+      { path: 'sedes', component: () => import('pages/SedesPage.vue'), meta: { permissions: ['sedes', 'gestionar_sedes', 'all'] } },
+      { path: 'roles', component: () => import('pages/RolesPage.vue'), meta: { permissions: ['roles', 'gestionar_roles', 'all'] } },
     ],
   },
   {
